dafa787a7c13e3a70bf7ebaffb1808bf6ca9219a
Malik Vance <malikovance@gmail.com>
2025-10-27 08:04:29 -0500

    firebase setup

---
+++ firbaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBlfxQBwAIZeXlQ3VpG4rZSLy2zoLo0qZI",
+  authDomain: "atlas-lumigram-3f535.firebaseapp.com",
+  projectId: "atlas-lumigram-3f535",
+  storageBucket: "atlas-lumigram-3f535.firebasestorage.app",
+  messagingSenderId: "654245998605",
+  appId: "1:654245998605:web:fd3411f8bd8cb4a8dd6544",
+  measurementId: "G-15E406Q931"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = initializeAuth(app);
